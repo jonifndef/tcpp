861b853982335cdc5f696ed31a1d843e7992fdf4
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2024-03-18 16:09:17 +0100

    Add small convenience script for testing arping

---
+++ scripts/test_arping.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cleanup()
+{
+    kill "${TCPP_PID}" > /dev/null 2>&1
+    kill "${ARPING_PID}" > /dev/null 2>&1
+}
+
+trap cleanup INT TERM EXIT
+
+if [ ! -d "build" ]; then
+    cmake -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+fi
+
+cmake --build build
+sudo setcap CAP_NET_ADMIN=eip build/tcpp
+
+./build/tcpp &
+TCPP_PID=$!
+
+sudo ip link set tap0 up
+sudo ip addr add 10.0.1.5/24 dev tap0
+sudo arping -I tap0 10.0.1.5 &
+ARPING_PID=$!
+
+sleep 5
