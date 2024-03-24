ab9bf40a0aba24feecbf05316968e66d1a2febc0
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2024-03-24 22:49:35 +0100

    Add lots of debug prints for addr

--- src/tap-device.cpp
+++ src/tap-device.cpp
@@ -5,6 +5,10 @@
 #include <fcntl.h>
 #include <linux/string.h>
 #include <sys/ioctl.h>
+#include <iomanip>
+
+#include <net/ethernet.h>
+#include <netinet/ether.h>
 
 namespace
 {
@@ -57,16 +61,70 @@ auto TapDevice::initialize(std::string dev) -> bool
 
 auto TapDevice::read_data() -> int
 {
-    int buffer[1000];
+    uint8_t buffer[1000];
+    std::ostream hexout(std::cout.rdbuf());
+    hexout << std::hex << std::uppercase;
 
     const int len = read(m_tap_fd, buffer, sizeof(buffer));
     if (len > 0)
     {
-        for (const auto byte : buffer)
+        std::cout << "len is: " << len << std::endl;
+
+        //for (const auto byte : buffer)
+        //{
+        //    hexout << static_cast<int>(byte);
+        //}
+
+        int i = 0;
+        for (; i < 6; i++)
         {
-            std::cout << std::hex << std::uppercase << byte;
+            hexout << static_cast<int>(buffer[i]);
+        }
+        std::cout << std::endl << std::endl;
+
+        for (; i < 12; i++)
+        {
+            hexout << static_cast<int>(buffer[i]);
+        }
+        std::cout << std::endl << std::endl;
+
+        for (; i < 14; i++)
+        {
+            hexout << static_cast<int>(buffer[i]);
         }
         std::cout << std::endl;
+
+        struct ether_addr des_eth_addr = {};
+        struct ether_addr src_eth_addr = {};
+        uint8_t des_addr[6];
+        uint8_t src_addr[6];
+        memcpy(des_addr, buffer, 6 * sizeof(uint8_t));
+        memcpy(src_addr, buffer + 6, 6 * sizeof(uint8_t));
+        for (int j = 0; j < 6; j++)
+        {
+            hexout << static_cast<int>(des_addr[j]);
+        }
+        std::cout << std::endl << std::endl;
+        for (int k = 0; k < 6; k++)
+        {
+            hexout << static_cast<int>(src_addr[k]);
+        }
+        std::cout << std::endl << std::endl;
+
+        memcpy(des_eth_addr.ether_addr_octet, des_addr, 6 * sizeof(uint8_t));
+        memcpy(src_eth_addr.ether_addr_octet, src_addr, 6 * sizeof(uint8_t));
+
+        std::cout << "hej" << std::endl;
+        hexout << static_cast<int>(des_eth_addr.ether_addr_octet[0]) << static_cast<int>(des_eth_addr.ether_addr_octet[1]) << std::endl;
+        hexout << static_cast<int>(src_eth_addr.ether_addr_octet[0]) << static_cast<int>(src_eth_addr.ether_addr_octet[1]) << std::endl;
+        std::cout << "hej då" << std::endl;
+
+        auto des_str = ether_ntoa(&des_eth_addr);
+        std::cout << "eth header formatted: " << des_str << std::endl;
+        auto src_str = ether_ntoa(&src_eth_addr);
+        std::cout << "eth header formatted: " << src_str << std::endl;
+
+        std::cout << std::endl << std::endl;
     }
 
     return len;
