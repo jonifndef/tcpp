6b691c0931cd665a60b962265760d45cbb94d61d
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2024-03-31 22:16:18 +0200

    Add argparse as a submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <unistd.h>
 
+#include "argparse/argparse.hpp"
+
 #include "tap-device.hpp"
+
+struct MyArgs : public argparse::Args
+{
+    int &num_packets = kwarg("n,num_packets", "Number of packet to receive before exiting").set_default(5);
+};
 
 int main(int argc, char **argv)
 {
+    auto args = argparse::parse<MyArgs>(argc, argv);
+
     TapDevice tap_dev{};
     tap_dev.initialize("tap0");
 
